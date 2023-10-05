873770dce733667eac21d7f6e716c7666a82c873
TakumaKouyama <granger33417@outlook.jp>
2023-10-06 00:29:06 +0900

    Merge branch 'master' of https://github.com/avocado336/Team_HIK.git

--- Team_HIK/src/main/java/com/example/demo/HelloController.java
+++ Team_HIK/src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 @RequestMapping("/hello")
 public String show() {
-return "こんばんわああああああ";
+return "こんばんわあああああああ";
 }
 }
